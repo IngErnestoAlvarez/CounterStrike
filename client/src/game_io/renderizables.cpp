1e54e15a151d76109e347da0b9f77c6b1f176a88
IngErnestoAlvarez <ernesto.alvarez1812@gmail.com>
2021-06-25 18:27:06 -0300

    Aniadida la clase renderizables

---
+++ client/src/game_io/renderizables.cpp
@@ -0,0 +1,76 @@
+#include "game_io/renderizables.h"
+
+#include "game_io/floor.h"
+#include "game_io/player.h"
+
+Renderizables::Renderizables(SdlWindow &window)
+    : texts(), objects(), window(&window) {
+    createTexts();
+    createObjects();
+}
+
+Renderizables::~Renderizables() {}
+
+void Renderizables::render() {
+    this->renderObjects();
+    this->renderTexts();
+}
+
+void Renderizables::createTexts() {
+    // using up = std::unique_ptr<SdlText>;
+    // this->texts.push_back(up(new SdlText(*this->window, "life 100/100")));
+    // this->texts.back()->set_pos(0, 500);
+    // this->texts.push_back(up(new SdlText(*this->window, "ammo 30/30")));
+    // this->texts.back()->set_pos(550, 500);
+}
+
+void Renderizables::createObjects() {
+    // using up = std::unique_ptr<SdlObject>;
+    // this->objects.push_back(
+    //     up(new Floor("assets/sprites/office.png", 1, *this->window)));
+    // this->objects.back()->set_init_pos(0, 0);
+    // this->objects.push_back(
+    //     up(new PlayerView("assets/sprites/ct2.png", 4, *this->window)));
+}
+
+void Renderizables::renderObjects() {
+    for (size_t i = 0; i < objects.size(); ++i) {
+        objects[i]->render();
+    }
+}
+
+void Renderizables::renderTexts() {
+    for (size_t i = 0; i < texts.size(); i++) {
+        texts[i]->render();
+    }
+}
+
+void Renderizables::moveUp() {
+    for (size_t i = 0; i < objects.size(); i++) {
+        objects[i]->moveUp();
+    }
+}
+
+void Renderizables::moveDown() {
+    for (size_t i = 0; i < objects.size(); i++) {
+        objects[i]->moveDown();
+    }
+}
+
+void Renderizables::moveLeft() {
+    for (size_t i = 0; i < objects.size(); i++) {
+        objects[i]->moveLeft();
+    }
+}
+
+void Renderizables::moveRight() {
+    for (size_t i = 0; i < objects.size(); i++) {
+        objects[i]->moveRight();
+    }
+}
+
+void Renderizables::mouseMove(int posX, int posY) {
+    for (size_t i = 0; i < objects.size(); i++) {
+        objects[i]->mouse_mov(posX, posY);
+    }
+}
