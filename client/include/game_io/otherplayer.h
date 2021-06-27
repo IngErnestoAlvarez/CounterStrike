211da517128623147814ab5e48286a1fbee30b39
IngErnestoAlvarez <ernesto.alvarez1812@gmail.com>
2021-06-27 18:44:55 -0300

    Aniadida la vista para otros jugadores

---
+++ client/include/game_io/otherplayer.h
@@ -0,0 +1,24 @@
+#ifndef __OTHERPLAYER_H__
+#define __OTHERPLAYER_H__
+
+#include "game_io/sdl/SdlObject.h"
+#include "game_logic/player.h"
+
+class OtherPlayer : public SdlObject {
+   private:
+    Player *player;
+    size_t animation_pos;
+
+   public:
+    OtherPlayer(std::string const &path, int animation_frames,
+                SdlWindow &window, Player *player);
+
+    ~OtherPlayer();
+
+    void render() override;
+
+   private:
+    void update_animation();
+};
+
+#endif  // __OTHERPLAYER_H__
---
+++ client/src/game_io/otherplayer.cpp
@@ -0,0 +1,33 @@
+#include "game_io/otherplayer.h"
+
+OtherPlayer::OtherPlayer(std::string const &path, int animation_frames,
+                         SdlWindow &window, Player *player)
+    : SdlObject(path, animation_frames, window),
+      player(player),
+      animation_pos(0) {
+    this->center = {16, 16};
+    this->pos = {200, 200};
+    this->sprite_clips[0].x = 0;
+    this->sprite_clips[0].y = 0;
+    this->sprite_clips[0].w = 32;
+    this->sprite_clips[0].h = 32;
+
+    this->sprite_clips[1].x = 32;
+    this->sprite_clips[1].y = 0;
+    this->sprite_clips[1].w = 32;
+    this->sprite_clips[1].h = 32;
+}
+
+OtherPlayer::~OtherPlayer() {}
+
+void OtherPlayer::update_animation() { animation_pos = 1 - animation_pos; }
+
+void OtherPlayer::render() {
+    SDL_Point pos_actual = {player->getX(), player->getY()};
+    if ((pos_actual.x != this->pos.x) || (pos_actual.y != this->pos.y)) {
+        this->update_animation();
+        this->pos = pos_actual;
+    }
+    this->image.render(pos_actual.x, pos_actual.y, this->player->getAngle(),
+                       center, &sprite_clips[animation_pos]);
+}
