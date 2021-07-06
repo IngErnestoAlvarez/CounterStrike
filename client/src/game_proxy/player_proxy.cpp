9ccd7259fcee5a11c139102c55237eeeba71ad47
IngErnestoAlvarez <ernesto.alvarez1812@gmail.com>
2021-07-06 17:32:27 -0300

    Solucion al angulo

--- client/src/game_proxy/player_proxy.cpp
+++ client/src/game_proxy/player_proxy.cpp
@@ -22,7 +22,8 @@ void PlayerProxy::setPlayer(char *data, size_t n) {
     money = int(::ntohs(*(uint16_t *)&data[2]));
     posx = int(::ntohs(*(uint16_t *)&data[4]));
     posy = int(::ntohs(*(uint16_t *)&data[6]));
-    angle = float(::ntohl(*(uint32_t *)&data[8]));
+    uint32_t angleAux = ::ntohl(*(uint32_t *)&data[8]);
+    angle = *(float *)&angleAux;
     equipped_weapon.setWeapon(int(*(uint8_t *)&data[12]));
     time = int(*(uint8_t *)&data[13]);
     gotBomb = bool(*(uint8_t *)&data[14]);
