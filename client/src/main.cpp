5f6efe6fd47515ea14f7da7fe033264a60e4c0aa
IngErnestoAlvarez <ernesto.alvarez1812@gmail.com>
2021-07-08 23:22:13 -0300

    Manejo de errores inesperados en el cliente

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -7,6 +7,8 @@
 #include "Logger.h"
 #include "app.h"
 
+#define UNEXPECTED_ERROR_ "Ocurrio un error inesperado, disculpe las molestias"
+
 int main(int argc, char const *argv[]) {
     using namespace CPlusPlusLogging;
     Logger *log = Logger::getInstance();
@@ -22,12 +24,16 @@ int main(int argc, char const *argv[]) {
 
     try {
         App game(argv[1], argv[2], argv[3]);
+
         log->info("Loading media");
         game.load_media();
+
         log->info("Starting main_loop");
         game.main_loop();
     } catch (const std::exception &e) {
-        std::cerr << e.what() << '\n';
+        std::cerr << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << UNEXPECTED_ERROR_ << std::endl;
     }
 
     log->info("Termina el programa");
