7781913a93e50ba6d15f8af4ff047679a4a94f9b
Matias <mhuenul@fi.uba.ar>
2021-07-13 01:32:45 -0300

    Se deshabilita debug en server por default

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
+#include <string>
 
 #include "Logger.h"
 #include "server.h"
 
-#define USAGE "Uso: ./<executable-filepath> <config-filepath>"
+#define USAGE "Uso: ./<executable-filepath> <config-filepath> [-debug]"
 #define UNEXPECTED_ERROR "Se ha producido un error inesperado"
 
 int main(int argc, char *argv[]) {
     using namespace CPlusPlusLogging;
     Logger *log = Logger::getInstance();
     log->enableConsoleLogging();
-    log->enaleLog();
-    // log->disableLog();
-    log->debug("Server");
 
-    if (argc != 2) {
+    if (argc > 3) {
         std::cout << USAGE << std::endl;
         return 1;
     }
+
+    std::string config_filepath = argv[1];
+    bool debug = (argc == 3)
+              && (std::string(argv[2]) == "-debug");
+
+    if (debug)
+        log->enaleLog();
 
     try {
         Server server(argv[1]);
