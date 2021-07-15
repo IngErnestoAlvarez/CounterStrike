d628f89b474c9d6c1e0269dc088ad990b4aefc7a
IngErnestoAlvarez <ernesto.alvarez1812@gmail.com>
2021-07-15 13:03:00 -0300

    Ajuste a commit anterior

--- client/src/game_io/final_match.cpp
+++ client/src/game_io/final_match.cpp
@@ -1,7 +1,8 @@
 #include "game_io/final_match.h"
 
 FinalMatchScreen::FinalMatchScreen(SdlWindow &window)
-    : winner(window, "TU EQUIPO HA GANADO", BLUE, 60),
+    : score(window),
+      winner(window, "TU EQUIPO HA GANADO", BLUE, 60),
       looser(window, "TU EQUIPO HA PERDIDO", RED, 60) {
     winner.set_pos(50, 100);
     looser.set_pos(50, 100);
